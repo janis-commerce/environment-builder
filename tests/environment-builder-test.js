49d9cdc37e713d414c33bfac9fe0d829d4ef02a9
gastonpereyra <rgpxen@gmail.com>
2019-06-26 18:35:08 -0300

    Added Featured to build environment

--- tests/environment-builder-test.js
+++ tests/environment-builder-test.js
@@ -4,10 +4,19 @@ const assert = require('assert');
 
 const sandbox = require('sinon').createSandbox();
 
-const { EnvironmentBuilder, EnvironmentBuilderError } = require('./../lib');
+const { EnvironmentBuilder } = require('./../lib');
 
 describe('EnvironmentBuilder', () => {
 
-	// your tests here...
+	context('when nothing is testing', () => {
+		it('should be ok', () => {
+			sandbox.stub(EnvironmentBuilder.prototype, 'execute').returns(true);
+
+			const envBuilder = new EnvironmentBuilder();
+
+			assert(envBuilder.execute());
+
+		});
+	});
 
 });
