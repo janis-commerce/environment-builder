fabafb7c3c8c93381b96c9a0404904689f7b2d14
gastonpereyra <gaston.pereyra@fizzmod.com>
2019-06-26 10:49:53 -0300

    Init Proyect

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const { EnvironmentBuilder } = require('./lib');
+
+module.exports = EnvironmentBuilder;
---
+++ tests/environment-builder-test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const assert = require('assert');
+
+const sandbox = require('sinon').createSandbox();
+
+const { EnvironmentBuilder, EnvironmentBuilderError } = require('./../lib');
+
+describe('EnvironmentBuilder', () => {
+
+	// your tests here...
+
+});
